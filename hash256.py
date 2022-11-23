607dc1c359ddd3c9b21c7efc636f0ff81c60bbf5
Hami1d <61083938+Hami1d@users.noreply.github.com>
2022-11-23 23:25:47 +0100

    Add files via upload

---
+++ hash256.py
@@ -0,0 +1,5 @@
+import hashlib as hash
+
+# print("sha3_384, sha1, sha384, shake_256, sha224, sha256, shake_128, sha3_512, sha512_224, sha512, md5, sha3_256, blake2s, sha512_256, sm3, md5-sha1, sha3_224, blake2b")
+word = hash.sha256(input('write your phrase here:').encode('utf-8')).hexdigest()
+print(f'sha256: {word}')
